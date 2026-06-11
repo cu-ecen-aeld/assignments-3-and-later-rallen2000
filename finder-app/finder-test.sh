dec0ce28b2dc9d8d640ff9c843b2a5a4dfda9d79
rallen8 <rallen8@Ubuntu.myguest.virtualbox.org>
2026-06-11 00:28:41 -0600

    BuildrootCommit5

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -52,8 +52,8 @@ then
 	fi
 fi
 #echo "Removing the old writer utility and compiling as a native application"
-make clean
-make CROSS_COMPILE="$CC" all
+#make clean
+#make CROSS_COMPILE="$CC" all
 
 for i in $( seq 1 $NUMFILES)
 do
